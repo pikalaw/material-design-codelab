84712f489584507a4f9b8203258c67997e27e542
Lawrence Tsang <pikalaw@gmail.com>
2020-01-31 23:51:33 -0500

    Completed the starter.

--- mdc-101/starter/lib/src/login_component.dart
+++ mdc-101/starter/lib/src/login_component.dart
@@ -1,9 +1,21 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+
+import 'route_paths.dart';
 
 @Component(
   selector: 'login',
   styleUrls: ['login_component.css'],
   templateUrl: 'login_component.html',
+  directives: [formDirectives],
 )
 class LoginComponent {
+  final Router _router;
+
+  LoginComponent(this._router);
+
+  void onSubmit() {
+    _router.navigate(RoutePaths.home.toUrl());
+  }
 }
